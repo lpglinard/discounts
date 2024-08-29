a0c9bcab800acf83786564854ed53ebda68cc726
Lucas Linard <lucas@linard.tech>
2024-08-29 09:11:54 -0300

    failing test

---
+++ src/test/java/com/example/order/LoyaltyServiceTest.java
@@ -0,0 +1,22 @@
+package com.example.order;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LoyaltyServiceTest {
+
+    LoyaltyService loyaltyService;
+
+    @BeforeEach
+    void setUp() {
+        loyaltyService = new LoyaltyService();
+    }
+
+    @Test
+    public void test_returnTrueIfClientIsMember() {
+        int id = 1;
+        loyaltyService.checkLoyaltyStatus();
+    }
+
+}
